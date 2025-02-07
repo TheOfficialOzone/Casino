ebec381a6f5f4f001b441dc4b00ada370387b9fb
Caelan <caelan.myskiw@gmail.com>
2025-02-07 11:34:52 -0600

    [#39] Add Basic view and controller for Lobby/casino hub page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  get "/lobby", to: "lobby#index"
   # Defines the root path route ("/")
   # root "posts#index"
 
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
 
   resources :users
+  root "lobby#index"
 end
