103f27507d6e09b77ef333fa00c034e08ff01bed
Caelan <caelan.myskiw@gmail.com>
2025-02-07 11:34:52 -0600

    [#39] Add Basic view and controller for Lobby/casino hub page

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,10 @@ Rails.application.routes.draw do
   get "/lobby", to: "lobby#index"
   post "/lobby/logout", to: "lobby#logout", as: :logout_lobby
   post "/lobby/horse_racing", to: "lobby#horse_racing", as: :horse_racing_lobby
+
+  resource :session
+  resources :passwords, param: :token
+
+  resources :users
+  root "lobby#index"
 end
