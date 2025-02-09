e203204210386ffe10a34cb83ef4a51cd75d2277
Niko Christie <nikolaaschristie@gmail.com>
2025-02-09 13:27:37 -0600

    Fix animations for Horses, Add timer
    Fixed horse animations so that they go off screen instead of reseting, added a timer so we know how long the race is taking

--- app/helpers/horse.rb
+++ app/helpers/horse.rb
@@ -1,8 +1,18 @@
 class Horse
+  @@speeds  = 5..10
+  @@timings = ["linear", "ease", "ease-in", "ease-out", "ease-in-out"]
+  
   attr_accessor :speed, :timing
 
   def initialize()
-    @speed = rand(1..10)
-    @timing = ["linear", "ease", "ease-in", "ease-out", "ease-in-out"].sample
+    seed = Random.new_seed
+    @r = Random.new(seed)
+    self.generate_race_parameters # randomize race parameters
+  end
+
+  # Randomize race parameters like `speed` and `timing` before a race
+  def generate_race_parameters()
+    @speed = @r.rand(@@speeds) + @r.rand    # Speed is random value in speed range + float to avoid ties
+    @timings = @@timings.sample(random: @r) # Pick a random timing based of random seed
   end
 end
