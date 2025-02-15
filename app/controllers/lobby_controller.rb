7ffd45a8f543abe60e32b2367b82be8a3321241c
Cole Chuchmach <chuchma1@myumanitoba.ca>
2025-02-15 15:17:12 -0600

    [#39] add authentication logic, logout, and horse racing redirecting

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -1,34 +1,19 @@
 class LobbyController < ApplicationController
-  before_action :authenticate_user
+  include Authentication
+
+  allow_unauthenticated_access
 
   def index
-    @user = User.find(session[:user_id]) if session[:user_id]
-    puts "Gambler #{@user.id} has entered the casino lobby" if @user
+    puts "A gambler has entered the casino lobby"
   end
 
   def logout
-    if session[:user_id]
-      puts "Gambler (ID: #{session[:user_id]}) is leaving the casino lobby."
-      reset_session # Clears the session completely
-    end
     flash[:notice] = "99% of gamblers quit before they win big, I see you're in the minority..."
-    redirect_to lobby_path # Once the path for the login is finished, replace it
   end
 
+  # When the horse racing button is pressed, the user is redirected to that games page.
   def horse_racing
-    if session[:user_id]
-      flash[:notice] = "I know you're eager to bet on some horses, however the stables aren't finished yet."
-      redirect_to lobby_path # Once the path for the horse racing is finished, replace it
-    else
-      flash[:alert] = "You must be logged in to play Horse Racing."
-      redirect_to login_path
-    end
-  end
-
-  private
-
-  def authenticate_user
-    puts "Attempting to authenticate the user..."
-    redirect_to login_path, alert: "Authentication Failed - Please Log In." unless session[:user_id]
+    flash[:notice] = "Bet on zoolander, he's my favourite!"
+    redirect_to home_path # Once the path for the horse racing is finished, replace it
   end
 end
