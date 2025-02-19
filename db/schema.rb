74821f85bbc07286f1545d672bc50fd0ece24724
Caelan <caelan.myskiw@gmail.com>
2025-02-19 12:43:23 -0600

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_250_212_055_215) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_12_055215) do
   create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "ip_address"
