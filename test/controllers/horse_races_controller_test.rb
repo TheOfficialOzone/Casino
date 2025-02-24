98ce2712d088aec751c0c0aa7398085328a66439
Owen Zonneveld <owenzonneveld@gmail.com>
2025-02-24 13:44:16 -0800

    Add additional tests for horse races, fix bug when removing too many horses

--- test/controllers/horse_races_controller_test.rb
+++ test/controllers/horse_races_controller_test.rb
@@ -18,15 +18,28 @@ class HorseRacesControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
-  def does_wager_pay(kind)
+  def does_wager_pay(kind, place)
     user = User.first
     user_id = user.id
     balance = user.balance
-    winner = Horse.order(:speed).first
+    winner = Horse.order(:speed)[place]
     place_bet({ horse: winner.id, kind: kind, amount: 1.00 })
     post resolve_race_path
 
     assert balance * winner.odds(kind) == User.find(user_id).balance
+  end
+
+  def does_wager_not_pay(kind, place)
+    user = User.first
+    user_id = user.id
+    balance = user.balance
+    winner = Horse.order(:speed)[place]
+    bet_amount = 1.00
+
+    place_bet({ horse: winner.id, kind: kind, amount: bet_amount })
+    post resolve_race_path
+
+    assert user.balance
   end
 
   test "should place show bet" do
@@ -54,32 +67,72 @@ class HorseRacesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should place multiple bets on different horses" do
+    login
     place_bet({ horse: Horse.first.id, kind: :show, amount: 0.0 })
     place_bet({ horse: Horse.second.id, kind: :place, amount: 0.0 })
 
     assert Wager.count == 2
   end
 
-  test "should pay user accordingly for sucessful show wager" do
+  test "should pay user accordingly for sucessful first placed horse show wager" do
     login
-    does_wager_pay :show
+    does_wager_pay :show, 0
+  end
+
+  test "should pay user accordingly for sucessful second placed horse show wager" do
+    login
+    does_wager_pay :show, 1
+  end
+
+  test "should pay user accordingly for sucessful third placed horse show wager" do
+    login
+    does_wager_pay :show, 2
+  end
+
+  test "should not pay user accordingly for horse placed out of the top 3 show wager" do
+    login
+    does_wager_not_pay :show, 3
+    does_wager_not_pay :show, 4
+    does_wager_not_pay :show, 5
   end
 
   test "should pay user accordingly for sucessful place wager" do
     login
-    does_wager_pay :place
+    does_wager_pay :place, 0
+  end
+
+  test "should pay user accordingly for sucessful second placed horse place wager" do
+    login
+    does_wager_pay :place, 1
+  end
+
+  test "should not pay user accordingly for horse placed out of the top 2 place wager" do
+    login
+    does_wager_not_pay :place, 2
+    does_wager_not_pay :place, 3
+    does_wager_not_pay :place, 4
+    does_wager_not_pay :place, 5
   end
 
   test "should pay user accordingly for sucessful straight wager" do
     login
-    does_wager_pay :straight
+    does_wager_pay :straight, 0
+  end
+
+  test "should not pay user accordingly for winning horse placed straight wager" do
+    login
+    does_wager_not_pay :straight, 1
+    does_wager_not_pay :straight, 2
+    does_wager_not_pay :straight, 3
+    does_wager_not_pay :straight, 4
+    does_wager_not_pay :straight, 5
   end
 
   test "should lose user money for a bad bet" do
     login
     user = User.first
     kind = :straight
-    amount = 1.00
+    amount = 1.0
 
     User.update(balance: amount)
 
@@ -91,5 +144,219 @@ class HorseRacesControllerTest < ActionDispatch::IntegrationTest
     user.reload
 
     assert user.balance
+  end
+
+  test "Creating extra horses" do
+    horse_count = Horse.count
+    Horse.create_new_horse()
+    assert Horse.count == horse_count + 1
+  end
+
+  test "Removing too many horses" do
+    Horse.remove_random_horses(amount = Horse.count + 100)
+    assert Horse.count == 0
+  end
+
+  test "Removing all horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+  end
+
+  test "Check all horses are eventually removed" do
+    starting_horses = Horse.all.uniq # Uniq is needed to clone the elements (Otherwise it's a pointer)
+
+    for _ in (1..200) do # Odds of this failing are 5/6^200... essentially 0
+      Horse.remove_random_horses(amount = 1)
+      Horse.create_new_horse()
+    end
+
+    assert (starting_horses | Horse.all).count == 12 # The starting horses and remaining horses are entirely unique
+  end
+
+  test "Remove Horse, Add Horse" do
+    horse_count = Horse.count
+    Horse.remove_random_horses(amount = 1)
+    assert Horse.count == horse_count - 1
+    Horse.create_new_horse()
+    assert Horse.count == horse_count
+  end
+
+  test "Regenerate all horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+  end
+
+  test "Generated Horses are unique to one-another" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+    # Checks the horses are unique
+    unique_horses = Horse.all.uniq
+    assert unique_horses.count == Horse.count
+  end
+
+  test "Generated Horses are unique to pre-existing horses" do
+    horse_count = Horse.count
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == horse_count + 6
+    # Checks the horses are unique
+    unique_horses = Horse.all.uniq
+    assert unique_horses.count == Horse.count
+  end
+
+  test "Betting on one generated horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    # Wagers are placed
+    login
+    place_bet({ horse: Horse.first.id, kind: :show, amount: 0.0 })
+
+    assert Wager.count == 1
+  end
+
+  test "Betting on multiple generated horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    # Wagers are placed
+    login
+    place_bet({ horse: Horse.first.id, kind: :show, amount: 0.0 })
+    place_bet({ horse: Horse.second.id, kind: :place, amount: 0.0 })
+    place_bet({ horse: Horse.third.id, kind: :straight, amount: 0.0 })
+
+    assert Wager.count == 3
+  end
+
+  test "Betting on one generated horse multiple times" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    # Wagers are placed
+    login
+    place_bet({ horse: Horse.first.id, kind: :show, amount: 0.0 })
+    place_bet({ horse: Horse.first.id, kind: :place, amount: 0.0 })
+    place_bet({ horse: Horse.first.id, kind: :straight, amount: 0.0 })
+
+    assert Wager.count == 3
+  end
+
+  test "Payout when betting & winning on generated horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    login
+
+    # Do all the bets on the horses
+    does_wager_pay :show, 0
+    does_wager_pay :show, 1
+    does_wager_pay :show, 2
+
+    does_wager_pay :place, 0
+    does_wager_pay :place, 1
+
+    does_wager_pay :straight, 0
+  end
+
+  test "No payout when betting & losing on generated horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    login
+
+    # Do all the bets on the horses
+    does_wager_not_pay :show, 3
+    does_wager_not_pay :show, 4
+    does_wager_not_pay :show, 5
+
+    does_wager_not_pay :place, 2
+    does_wager_not_pay :place, 3
+    does_wager_not_pay :place, 4
+    does_wager_not_pay :place, 5
+
+    does_wager_not_pay :straight, 1
+    does_wager_not_pay :straight, 2
+    does_wager_not_pay :straight, 3
+    does_wager_not_pay :straight, 4
+    does_wager_not_pay :straight, 5
+  end
+
+  test "Hit & Miss bet on generated horses" do
+    Horse.remove_random_horses(amount = Horse.count)
+    assert Horse.count == 0
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    Horse.create_new_horse()
+    assert Horse.count == 6
+
+    login
+
+    # Do all the bets on the horses
+    does_wager_pay :show, 2
+
+    does_wager_pay :place, 0
+    does_wager_pay :place, 1
+
+    does_wager_pay :straight, 0
+
+    does_wager_not_pay :show, 3
+    does_wager_not_pay :place, 3
+    does_wager_not_pay :straight, 1
   end
 end
